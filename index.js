378499d2d318bd72c0c3128aea85628f09625631
ciaranj <ciaranj@gmail.com>
2014-02-13 17:24:50 +0000

    Misc. fixes required for statsd ceres backend.

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 exports.CeresTree = require("./lib/cerestree").CeresTree;
 exports.CeresSlice = require("./lib/ceresslice").CeresSlice;
+exports.CeresNode = require("./lib/ceresnode").CeresNode;
+exports.CeresErrors = require("./lib/cereserrors").CeresErrors;
+exports.CeresUtils = require("./lib/ceresutils");
